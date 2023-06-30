99b4394f7d48fbd142a7c9c1f48346c984b6b9f3
Pastafredda <justspotify09@gmail.com>
2023-06-30 17:38:56 +0200

    bonus 1

--- index.php
+++ index.php
@@ -1,20 +1,39 @@
 <?php
+class Genre{
+    public $name;
+    public function __construct($name){
+        $this -> name = $name;
+    }
+
+}
 class Movie {
     public $title;
     public $director;
-    public $plot;
+    public array $genres;
 
-    public function __construct($title,$director,$plot){
+    public function __construct($title,$director, Genre ...$genres){
         $this -> title = $title;
         $this -> director = $director;
-        $this -> plot = $plot;
+        $this -> genres = $genres;
     }
 
     public function getFullmovie(){
-        return  $this -> title . " " . $this -> director . " " . $this -> plot;
+        $movie = $this -> title . " " . $this -> director;
+        foreach($this -> genres as $genre){
+            $movie .=  " " .$genre -> name;
+        }
+        return $movie;
     }
+
 }
 
+$genre1 = new Genre("Crime/Drama");
+$genre2 = new Genre("Mystery");
 
-$movie1 = new Movie("Carlito's Way", "Brian De Palma", "Carlito Brigante chooses to lead a quiet life after being released from prison.");
-var_dump($movie1);
+
+$movie1 = new Movie("Carlito's Way", "Brian De Palma" , $genre1);
+$movie2 = new Movie ("Zodiac", "David Fincher",  $genre2);
+echo $movie1 -> getFullmovie();
+
+echo "<br />";
+echo $movie2-> getFullmovie();
